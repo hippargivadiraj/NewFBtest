2963c3d3120674f0718b2029a72f7c92f6015a46
Vadiraj Hippargi <Vadi@leadconsultant.com>
2020-12-22 10:37:48 -0800

    Initial Commit

---
+++ NewFBtest/NewFBtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewFBtestApp.swift
+//  NewFBtest
+//
+//  Created by Vadiraj Hippargi on 12/22/20.
+//
+
+import SwiftUI
+
+@main
+struct NewFBtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
